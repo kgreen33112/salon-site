7c679bd686bca101b49c4d7573e72705c6efb1f5
kgreen33112 <kgreencoprods@gmail.com>
2026-05-19 19:44:18 -0500

    Updated paths

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const bookBtn = document.getElementById('bookBtn');
 const form = document.querySelector('.form-container');
 
 bookBtn.addEventListener('click', () => {
-    window.location.href = '../pages/appointment.html';
+    window.location.href = './pages/appointment.html';
 });
